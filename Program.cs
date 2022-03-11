6f9e8d7d0b07a242445aa0ba7933e6580a60f2e5
mahmood6889 <mahmood_sh6889@yahoo.com>
2022-03-11 14:28:13 +0330

    soal3

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+int month;
+
+System.Console.WriteLine("write number");
+month=Convert.ToInt32( Console.ReadLine());
+
+
+switch (month)
+  {
+      
+        case 1:
+          Console.WriteLine("januray");
+          break;
+        case 2:
+          Console.WriteLine("februry");
+          break;
+        case 3:
+          Console.WriteLine("march");
+          break;
+        case 4:
+          Console.WriteLine("april");
+          break;
+        case 5:
+          Console.WriteLine("may");
+          break;
+        case 6:
+          Console.WriteLine("june");
+          break;
+          case 7:
+          Console.WriteLine("july");
+          break;
+        case 8:
+          Console.WriteLine("august");
+          break;
+        case 9:
+          Console.WriteLine("september");
+          break;
+        case 10:
+          Console.WriteLine("october");
+          break;
+        case 11:
+          Console.WriteLine("november");
+          break;
+        case 12:
+          Console.WriteLine("december");
+          break;
+          default:System.Console.WriteLine("nadarim dadsh");
+        break;
+      }    
